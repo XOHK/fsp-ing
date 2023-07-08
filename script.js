b3a37f1f9aa4c5d2afeab5746a90f3edc1c74ae1
Ibrahim Doskhoev <ibrahim.doskhoev@gmail.com>
2023-07-08 16:55:05 +0300

    last commit :)

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-document.addEventListener("DOMContentLoaded", function() { 
-    var scrollTopButton = document.querySelector(".scroll-top"); 
-    window.addEventListener("scroll", function() { 
-        if (window.pageYOffset > 200) { 
-            scrollTopButton.style.display = "block"; 
-        } else { 
-            scrollTopButton.style.display = "none"; 
-        } 
-    }); 
-});
+const btn = document.getElementById("btn");
+const span = document.getElementById("modalClose");
+
+btn.onclick = () => {
+  document.getElementById("modal").style.display = "block";
+};
+
+window.onclick = (event) => {
+  if (event.target.id == "modal") {
+    document.getElementById("modal").style.display = "none";
+  }
+};
+
+const sendMessage = () => {
+  const name = document.getElementById("messageName");
+  const email = document.getElementById("messageEmail");
+  const message = document.getElementById("message");
+  alert(
+    `Сообщение отправлено!\nИмя: ${name.value}\nEmail: ${email.value}\nСообщение: ${message.value}`
+  );
+  document.getElementById("modal").style.display = "none";
+};
